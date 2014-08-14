e0b726c4e53f567dd2a00dadf86e3ff7de07ecc4
Dylan.Wen <hhkbp2@gmail.com>
2014-08-14 16:12:27 +0800

    add tcp server addr

---
+++ persist.go
@@ -0,0 +1,67 @@
+package rafted
+
+import (
+    "github.com/hhkpb2/rafted/persist"
+    "net"
+)
+
+type TCPServerAddr struct {
+    *net.TCPAddr
+}
+
+func (self *TCPServerAddr) Encode() ([]byte, error) {
+    return []byte(addr.String()), nil
+}
+
+func (self *TCPServerAddr) Decode([]byte) error {
+    return net.ResolveTCPAddr("", string(addr))
+}
+
+func EncodeServerAddrs(addrs []persist.ServerAddr) ([]byte, error) {
+    if addrs == nil {
+        return nil, nil
+    }
+    var result bytes.Buffer
+    for _, addr := range addrs {
+        addrBin, err := add.Encode()
+        if err != nil {
+            return nil, errors.New(
+                fmt.Sprintf("fail to encode addr: %s", addr.String()))
+        }
+        if _, err := result.Write(addrBin); err != nil {
+            return nil, err
+        }
+    }
+    return result.Bytes(), nil
+}
+
+func EncodeConfig(conf *persist.Config) (*persist.Configuration, error) {
+    if conf == nil {
+        return nil, nil
+    }
+    ServersBin, err := EncodeServerAddrs(conf.Servers)
+    if err != nil {
+        return nil, err
+    }
+    NewServersBin, err := EncodeServerAddrs(conf.NewServers)
+    if err != nil {
+        return nil, err
+    }
+    conf := &persist.Configuration{
+        Servers:    ServersBin,
+        NewServers: NewServersBin,
+    }
+    return conf, nil
+}
+
+func IsNormalConfig(conf *persist.Config) bool {
+    return (conf.Servers != nil) && (conf.NewServers == nil)
+}
+
+func IsOldNewConfig(conf *persist.Config) bool {
+    return (conf.Servers != nil) && (conf.NewServers != nil)
+}
+
+func IsNewConfig(conf *persist.Config) bool {
+    return (conf.Server == nil) && (conf.NewServers != nil)
+}
--- util.go
+++ util.go
@@ -4,7 +4,7 @@ import (
     "errors"
     "fmt"
     ev "github.com/hhkbp2/rafted/event"
-    "github.com/hhkbp2/rafted/persist"
+    ps "github.com/hhkbp2/rafted/persist"
     "net"
     "sync"
 )
@@ -73,54 +73,6 @@ func (self *ClientEventListener) Stop() {
     self.group.Wait()
 }
 
-func EncodeAddr(addr net.Addr) ([]byte, error) {
-    if addr == nil {
-        return nil, nil
-    }
-    return []byte(addr.String()), nil
-}
-
-func DecodeAddr(addr []byte) (net.Addr, error) {
-    return net.ResolveTCPAddr("", string(addr))
-}
-
-func EncodeAddrs(addrs []net.Addr) ([]byte, error) {
-    if addrs == nil {
-        return nil, nil
-    }
-    var result bytes.Buffer
-    for _, addr := range addrs {
-        addrBin, err := EncodeAddr(addr)
-        if err != nil {
-            return nil, errors.New(
-                fmt.Sprintf("fail to encode addr: %s", addr.String()))
-        }
-        if _, err := result.Write(addrBin); err != nil {
-            return nil, err
-        }
-    }
-    return result.Bytes(), nil
-}
-
-func EncodeConfig(conf *persist.Config) (*persist.Configuration, error) {
-    if conf == nil {
-        return nil, nil
-    }
-    ServersBin, err := EncodeAddrs(conf.Servers)
-    if err != nil {
-        return nil, err
-    }
-    NewServersBin, err := EncodeAddrs(conf.NewServers)
-    if err != nil {
-        return nil, err
-    }
-    conf := &persist.Configuration{
-        Servers:    ServersBin,
-        NewServers: NewServersBin,
-    }
-    return conf, nil
-}
-
 // Min returns the minimum.
 func Min(a, b uint64) uint64 {
     if a <= b {
@@ -137,20 +89,10 @@ func Max(a, b uint64) uint64 {
     return b
 }
 
-func IsNormalConfig(conf *persist.Config) bool {
-    return (conf.Servers != nil) && (conf.NewServers == nil)
-}
+func MapSetMinus(
+    s1 map[ps.ServerAddr]*Peer, s2 map[ps.ServerAddr]*Peer) []ps.ServerAddr {
 
-func IsOldNewConfig(conf *persist.Config) bool {
-    return (conf.Servers != nil) && (conf.NewServers != nil)
-}
-
-func IsNewConfig(conf *persist.Config) bool {
-    return (conf.Server == nil) && (conf.NewServers != nil)
-}
-
-func MapSetMinus(s1 map[net.Addr]*Peer, s2 map[net.Addr]*Peer) []net.Addr {
-    diff := make([]net.Addr, 0)
+    diff := make([]ps.ServerAddr, 0)
     for addr, _ := range s1 {
         if _, ok := s2[addr]; !ok {
             diff = append(diff, addr)
